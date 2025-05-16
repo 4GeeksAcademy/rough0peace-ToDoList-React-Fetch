568a27ca9b3c4b77b328a4704f532cf9ef081174
Ian Garvey <ianarthurgarvey@gmail.com>
2025-05-16 20:04:19 +0000

    can now add todo lists with enter key or button, delete tasks with icon. Need to render length of item array for list of tasks at bottom and make it so that when mouse hover, delete icon appears

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,16 +6,27 @@ import ToDoList from "../../js/components/ToDoList.jsx";
 //create your first component
 const Home = () => {
   return (
-    <div className="page-body d-flex justify-content-center">
+  //   <div className="page-body d-flex justify-content-center">
+  //     <div className="title">
+  //       <div className="list-body d-flex-column text-center">
+  //         <h1>todos</h1>
+  //       </div>
+  //       <ToDoList />
+  //     </div>
+  //   </div>
+  // );
+    <div className="container">
       <div className="title">
-        <div className="list-body d-flex-column text-center">
-          <h1>todos</h1>
-        </div>
-        <ToDoList />
+        <h1>To Do List</h1>
+      </div>
+      <ul>
+        <li><ToDoList /></li>
+      </ul>
+      <div className="number-of-tasks">
+        # tasks 
       </div>
     </div>
-  );
-
+  )
 };
 
 export default Home;
--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -4,7 +4,7 @@
 //=====================
 
 // Your app needs to look like this.
-// The tasks are added when the user presses enter on the keyboard, or you can have your own button.
+// The tasks are added when the user presses enter on the keyboard, or you can have your own button. [check]
 // The delete icon shows only when the task is hovered.
 // The user can add as many tasks as they want.
 // When there are no tasks the list should say "No tasks, add a task"
@@ -13,19 +13,26 @@
 import React, { useState, useEffect } from "react";
 
 function ToDoList() {
-  const [newTask, setNewTask] = useState("");
-  const [list, setList] = useState(["make the bed", "wash dishes"]);
+  const [newTask, setNewTask] = useState("");     
+  const [list, setList] = useState([]);
 
-  function handleUserKeyPress(event) {
+  const handleUserKeyPress = async (event) => {
     if (event.key === "Enter") {
-      console.log("add task");
       event.stopPropagation();
       event.preventDefault();
-      setList([...list, newTask]);
-      setNewTask("");
+      callAddToDo();
     }
-  }
-
+  };
+  const callAddToDo = async () => {
+    await handleAddingToDo();
+    setNewTask("");
+    await getToDos();
+  };
+  const handleDelete = async (id) => {
+    await deleteToDo(id);
+    await getToDos();
+  };
+  
   async function handleAddingToDo() {
     const task = {
       label: newTask,
@@ -41,18 +48,48 @@ function ToDoList() {
         },
       }
     );
-    const data = await response.json();
+    const data = await response.json(); 
     console.log(data);
   }
 
   const getToDos = async () => {
-
     const response = await fetch(
-      "https://playground.4geeks.com/todo/users/rough0peace"
+      "https://playground.4geeks.com/todo/users/rough0peace",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
     );
+
+    if (response.status === 404) {
+      console.log("Error fetching todos");
+      await fetch("https://playground.4geeks.com/todo/users/rough0peace", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return getToDos();
+    }
+
     const data = await response.json();
     setList(data.todos);
-    return data.todos;
+    console.log("Here's the get todos", data.todos);
+    return data;
+  };
+
+  const deleteToDo = async (id) => {
+    const response = await fetch(
+      `https://playground.4geeks.com/todo/todos/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   };
 
   useEffect(() => {
@@ -61,25 +98,21 @@ function ToDoList() {
 
   return (
     <div className="list-body">
-      <input
-        type="text"
-        value={newTask}
-        onKeyDown={handleUserKeyPress}
-        onChange={(event) => setNewTask(event.target.value)}
-      ></input>
-      <ul className="list-group list-group-flush">
-        {list.map((task, index) => {
+      <div className="input-container">
+        <input
+          type="text"
+          value={newTask}
+          placeholder="No tasks, add a task!"
+          onKeyDown={handleUserKeyPress}
+          onChange={(event) => setNewTask(event.target.value)}
+        ></input>
+        <button onClick={() => callAddToDo()} className="btn btn-success">
+          add
+        </button>
+      </div>
+      <ul>
+        {list?.map((task, index) => {
           return (
-            //event listener li onclick delete
-            //onClick={(event) => {
-
-            //filter list using the index of task
-            //}}
-            //filter list using the index of task
-            //onClick={(event) => {
-            //const filteredArray = list.filter((task, i) => index !== i);
-            // setList(filteredArray);
-            //}}
             <li
               className="list-group-item d-flex justify-content-between"
               key={index}
@@ -89,6 +122,7 @@ function ToDoList() {
                 type="button"
                 className="btn-close"
                 aria-label="Close"
+                onClick={() => handleDelete(task.id)}
               ></button>
             </li>
           );
